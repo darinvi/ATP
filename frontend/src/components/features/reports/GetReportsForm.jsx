2527bbaabd9df5f8b7286670c7e9dcb1210f4cc8
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-10-08 23:13:21 +0300

    reports reducer fixed expired token msg

--- frontend/src/components/features/reports/GetReportsForm.jsx
+++ frontend/src/components/features/reports/GetReportsForm.jsx
@@ -34,7 +34,6 @@ export default function GetReportForm(props) {
     function handleSubmitForm(e) {
         e.preventDefault();
         props.disableForm(false)
-        dispatch(loadAccounts(localStorage.getItem('reportToken')))
     }
 
     return (
