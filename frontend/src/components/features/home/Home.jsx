53d5a484be2588d3e88fa3f618623ecc6eae11bc
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-11-26 11:56:18 +0200

    wip

--- frontend/src/components/features/home/Home.jsx
+++ frontend/src/components/features/home/Home.jsx
@@ -17,15 +17,10 @@ export default function Home() {
         dispatch(setLastPage("home"));
     }, [])
 
-    // FilingRow clicked form the Home page to go to Filing page and render the html cliced. <------------- 1
     // On click of the date to render a bar chart of the ticker for that date.
-    // On click of the user 
     // Add market holidays
     // Trade ideas (with dinamic variables), can be commented by the mentors
-    // Add some components that summarize the other pages (filings list, mentor panel, etc) 
     // Dont call all playbooks, only render a couple, then fetch more if scrolled all teh way down?
-    // Press on playbook to render playbook form page
-    // Mark posts as read is a good way to keep the page simple and not have to worry about rendering tons of playbooks (even ones already read)
     return (
         <div className="flex">
             <div
