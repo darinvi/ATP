b3caff63efae448143083f2c0b3fbacffc7a31ad
darinvi <d.v.ivanov98@gmail.com>
2024-12-06 13:49:46 +0200

    wip create tag reports. TODO create strategies

--- frontend/src/components/features/reports/filters/CreateTag.jsx
+++ frontend/src/components/features/reports/filters/CreateTag.jsx
@@ -13,17 +13,16 @@ export default function CreateTag({ setShowBuilders }) {
     const [name, setName] = useState("")
     const [description, setDescription] = useState("");
     const [isActive, setIsActive] = useState(false);
+    const [chooseStrategyActive, setChooseStrategyActive] = useState(false);
 
     const inputCLass = "bg-cyan-700 hover:bg-cyan-800 focus:bg-cyan-800 text-white px-3 rounded";
     const disabledClass = "disabled:opacity-20 disabled:scale-100 disabled:bg-gray-900 disabled:text-gray-300";
     const buttonClass = "rounded px-2 hover:text-black border border-cyan-700 transform active:scale-95";
 
-    function modalChildren() {
+    function modalCreateTag() {
         return (
             <div
-                className="bg-cyan-900 rounded-lg shadow-lg p-6 w-full max-w-md"
-
-                onClick={e => e.stopPropagation()}
+                className={`bg-cyan-900 shadow-lg p-6 w-full max-w-md ${chooseStrategyActive ? 'rounded-l-lg' : 'rounded-lg'}`}
             >
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col gap-6 w-full">
@@ -41,21 +40,26 @@ export default function CreateTag({ setShowBuilders }) {
                                     onChange={e => setName(e.target.value)}
                                 />
                             </div>
-                            <button className="flex gap-2 items-center hover:text-green-200">
-                                <span>Add to Strategy</span>
-                                <svg
-                                    className={`transform transition-transform duration-150`}
-                                    width={12}
-                                    height={12}
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
+                            {!chooseStrategyActive &&
+                                <button
+                                    className="flex gap-2 items-center hover:text-green-200 active:scale-95"
+                                    onClick={() => setChooseStrategyActive(true)}
                                 >
-                                    <path d="M2 12L22 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-                                    <path d="M22 12L16 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-                                    <path d="M22 12L16 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-                                </svg>
-                            </button>
+                                    <span>Add to Strategy</span>
+                                    <svg
+                                        className={`transform transition-transform duration-150`}
+                                        width={12}
+                                        height={12}
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M2 12L22 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                                        <path d="M22 12L16 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                                        <path d="M22 12L16 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                                    </svg>
+                                </button>
+                            }
                         </div>
                     </div>
 
@@ -73,7 +77,7 @@ export default function CreateTag({ setShowBuilders }) {
                         />
                     </div>
                 </div>
-                <div className="flex gap-2 justify-end">
+                <div className="flex gap-2 justify-end pt-4">
                     <button
                         disabled={!name}
                         className={`${disabledClass} ${buttonClass} hover:bg-green-200 px-4`}
@@ -89,6 +93,43 @@ export default function CreateTag({ setShowBuilders }) {
                     />
                 </div>
             </div>
+        )
+    }
+
+    function modalChooseStrategy() {
+        return (
+            <div className="bg-cyan-900 flex flex-col gap-4 rounded-r-lg p-6 justify-between">
+                <p>whaaaaaaaaaaaat</p>
+                {/* TODO: <p>each strategy to be an element that sends a request for adding a tag, 
+                    until a response have loading animation, when succesfull have the tick</p> */}
+                <div className="flex justify-end">
+                    <svg
+                        className={`transform transition-transform duration-150 hover:text-red-200 hover:scale-125`}
+                        width={18}
+                        height={18}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        onClick={() => setChooseStrategyActive(false)}
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M22 12L2 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                        <path d="M2 12L8 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                        <path d="M2 12L8 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                    </svg>
+                </div>
+            </div>
+        )
+    }
+
+    function modalChildren() {
+        return (
+            <div
+                className="flex"
+                onClick={e => e.stopPropagation()}
+            >
+                {modalCreateTag()}
+                {chooseStrategyActive && modalChooseStrategy()}
+            </div >
         )
     }
 
